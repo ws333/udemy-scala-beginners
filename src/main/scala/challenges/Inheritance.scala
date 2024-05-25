e99d24eb1692c6d2a3d103b085dfe16dc61fa418
Bjørnar Hvidsten <devenania@proton.me>
2024-05-25 17:45:06 +0200

    make list generic

--- src/main/scala/challenges/Inheritance.scala
+++ src/main/scala/challenges/Inheritance.scala
@@ -4,14 +4,13 @@ import scala.annotation.tailrec
 
 object Inheritance extends App {
 
-  case class Node(i: Int) {
-    val value: Int = i
-    var next: Node | Null = null
+  case class Node[T](i: T) {
+    val value: T = i
+    var next: Node[T] | Null = null
 
     // @tailrec
     def getElementsAsString(acc: String = ""): String = {
       val newValue = acc + ", " + value
-
       if (next == null) newValue
       else {
         val a = if (acc == "") value.toString() else newValue
@@ -22,18 +21,18 @@ object Inheritance extends App {
     override def toString: String = s"[${getElementsAsString()}]"
   }
 
-  abstract class MyList {
-    var head: Node
-    var tail: Node
+  abstract class MyList[T] {
+    var head: Node[T]
+    var tail: Node[T]
     def isEmpty: Boolean
-    def add(e: Int): Unit
+    def add(e: T): Unit
   }
 
-  class ListA(i: Int = 0) extends MyList {
+  class ListA[T](i: T) extends MyList[T] {
     var head = new Node(i)
-    var tail: Node = head
+    var tail = head
     def isEmpty: Boolean = head == null
-    def add(e: Int) = {
+    def add(e: T) = {
       tail.next = new Node(e)
       tail = tail.next
     }
@@ -58,10 +57,17 @@ object Inheritance extends App {
     // }
   }
 
-  var aList = new ListA(333)
+  var aList = new ListA[Int](333)
 
   aList.add(777)
   aList.add(999)
 
   println(aList.toString())
+
+  var sList = new ListA[String]("I")
+
+  sList.add("am")
+  sList.add("a list of strings")
+
+  println(sList.toString())
 }
