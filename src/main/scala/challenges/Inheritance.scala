3a9fe21b2f093a34b80e8633a125e8aa97b8e48f
Bjørnar Hvidsten <devenania@proton.me>
2024-05-25 02:01:50 +0200

    add inheritance solution

---
+++ src/main/scala/challenges/Inheritance.scala
@@ -0,0 +1,62 @@
+package challenges
+
+import scala.annotation.tailrec
+
+object Inheritance extends App {
+
+  case class Node(i: Int) {
+    val value: Int = i
+    var next: Node | Null = null
+
+    def getElementsAsString: String = {
+      if (next == null)
+        value.toString()
+      else
+        value + ", " + next.getElementsAsString
+    }
+
+    override def toString: String = s"[$getElementsAsString]"
+  }
+
+  abstract class MyList {
+    var head: Node
+    var tail: Node
+    def isEmpty: Boolean
+    def add(e: Int): Unit
+  }
+
+  class ListA(i: Int = 0) extends MyList {
+    var head = new Node(i)
+    var tail: Node = head
+    def isEmpty: Boolean = head == null
+    def add(e: Int) = {
+      tail.next = new Node(e)
+      tail = tail.next
+    }
+
+    override def toString: String = {
+      head.toString()
+    }
+
+    // override def toString: String = {
+
+    //   val c = head.next
+    //   val a = head.value.toString()
+
+    //   @tailrec
+    //   def trScanList(current: Node = c, acc: String = a): String = {
+    //     if (current == null) return acc
+    //     if (current.next == null) return acc + ", " + current.value
+    //     else trScanList(current.next, acc + ", " + current.value)
+    //   }
+
+    //   trScanList()
+    // }
+  }
+
+  var aList = new ListA(333)
+  aList.add(777)
+  aList.add(999)
+
+  println(aList.toString())
+}
