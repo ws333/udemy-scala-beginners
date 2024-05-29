d76bd4052eb25bef07a741d2ee8c9f340aadc992
Bjørnar Hvidsten <devenania@proton.me>
2024-05-29 21:01:55 +0200

    add example for given

---
+++ src/main/scala/challenges/Given.scala
@@ -0,0 +1,31 @@
+package challenges
+
+import scala.compiletime.ops.int
+
+object MyGiven extends App {
+  trait Ord[T]:
+    def compare(x: T, y: T): Int
+    extension (x: T)
+      def <(y: T) = compare(x, y) < 0
+      def >(y: T) = compare(x, y) > 0
+
+  given intOrd: Ord[Int] with
+    def compare(x: Int, y: Int) =
+      if x < y then -1 else if x > y then +1 else 0
+
+  given listOrd[T](using ord: Ord[T]): Ord[List[T]] with
+
+    def compare(xs: List[T], ys: List[T]): Int = (xs, ys) match
+      case (Nil, Nil) => 0
+      case (Nil, _)   => -1
+      case (_, Nil)   => +1
+      case (x :: xs1, y :: ys1) =>
+        val fst = ord.compare(x, y)
+        if fst != 0 then fst else compare(xs1, ys1)
+
+  val intCompare = intOrd.compare(5, 2)
+  println(intCompare)
+
+  val listCompare = listOrd.compare(List(2, 2, 7), List(2, 3, 2))
+  println(listCompare)
+}
